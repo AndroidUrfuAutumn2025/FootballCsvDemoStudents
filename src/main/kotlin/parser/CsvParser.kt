f5bcc2555e911d49a84f2c8b5701bbef1a163476
StepanovR <krominus3@gmail.com>
2025-09-13 20:32:25 +0500

    sdelal

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,26 @@
 package parser
 
-object CsvParser
+import model.Player
+import model.Team
+import java.io.File
+
+object CsvParser{
+    fun playersParsing(src: String): MutableList<Player> {
+        val players = mutableListOf<Player>()
+
+        File(src)
+            .readLines()
+            .drop(1)
+            .forEachIndexed { index, line ->
+                val row = line.split(";")
+                if (row.size == 12) {
+                    players.add(Player(row))
+                } else {
+                    println("Строка ${index + 1} пропущена: $row")
+                }
+            }
+
+        return players
+    }
+
+}
--- src/main/kotlin/resolver/Resolver.kt
+++ src/main/kotlin/resolver/Resolver.kt
@@ -1,3 +1,55 @@
 package resolver
 
-class Resolver
+import model.Player
+import model.Team
+
+class Resolver(val players: List<Player>): IResolver{
+    // Выведите количество игроков, интересы которых не представляет агенство.
+    override fun getCountWithoutAgency(): Int{
+        return players.count { it.agency.isEmpty() }
+    }
+
+    // Выведите автора наибольшего числа голов из числа защитников и их количество.
+    override fun getBestScorerDefender(): Pair<String, Int> {
+        val defender = players.filter { it.position == "DEFENDER" }.maxByOrNull { it.goals }
+
+        return if (defender != null) {
+            defender.name to defender.goals
+        } else {
+            "Нет защитников" to 0
+        }
+    }
+
+    // Выведите русское название позиции самого дорогого немецкого игрока.
+    override fun getTheExpensiveGermanPlayerPosition(): String{
+        val player = players.filter { it.nationality == "Germany" }.maxByOrNull { it.transferCost }
+
+        val positionsToRus: Map<String, String> = mapOf(
+            "DEFENDER" to "Защитник",
+            "FORWARD" to "Нападающий",
+            "GOALKEEPER" to "Вратарь",
+            "MIDFIELD" to "Полузащитник"
+        )
+
+        return if (player != null) {
+            positionsToRus.getOrDefault(player.position, "Такой позиции не существует")
+        } else {
+            "Немецкие игроки не найдены"
+        }
+    }
+
+    // Выберите команду с наибольшим числом удалений на одного игрока.
+    override fun getTheRudestTeam(): Team {
+        val teams = players.groupBy { it.team }.map { (teamName, players) -> Team(teamName, players) }
+        return teams.maxBy { team -> team.players.map {it.redCards}.average() }
+    }
+
+    fun getTop10TeamsByTransferCost(): List<Pair<String, Int>>{
+        val topTeams = players.groupBy { it.team }
+            .mapValues { (_, players) -> players.sumOf { it.transferCost } }
+            .toList().sortedByDescending { it.second }.take(10)
+
+        return topTeams
+    }
+
+}
