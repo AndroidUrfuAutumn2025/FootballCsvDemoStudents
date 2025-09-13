b3e787715c75019e374cb4df1f0a78f32577d7be
Mikhail Gavrilyak <gavrilyak.mikhail@gmail.com>
2025-09-13 16:56:58 +0500

    правка

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,12 +7,12 @@ const val PLAYERS_FILE_PATH = "src/main/resources/fakePlayers.csv"
 fun main() {
     val players = CsvParser.parsePlayers(PLAYERS_FILE_PATH)
 
-    val resolver = Resolver(players)
-
-    println("Количество игроков, интересы которых не представляет агенство: \"${resolver.getCountWithoutAgency()}\"")
-    println("Автор наибольшего числа голов из числа защитников и их количество: \"${resolver.getBestScorerDefender()}\"")
-    println("Русское название позиции самого дорогого немецкого игрока: \"${resolver.getTheExpensiveGermanPlayerPosition()}\"")
-    println("Команда с наибольшим средним числом красных карточек на одного игрока: \"${resolver.getTheRudestTeam().name}\"")
+    Resolver(players).let {
+        println("Количество игроков, интересы которых не представляет агенство: \"${it.getCountWithoutAgency()}\"")
+        println("Автор наибольшего числа голов из числа защитников и их количество: \"${it.getBestScorerDefender()}\"")
+        println("Русское название позиции самого дорогого немецкого игрока: \"${it.getTheExpensiveGermanPlayerPosition()}\"")
+        println("Команда с наибольшим средним числом красных карточек на одного игрока: \"${it.getTheRudestTeam().name}\"")
+    }
 
     ChartDrawer.drawPositionsPieChart(players)
 }
