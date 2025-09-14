cd86eec39f4492844b391642f683dc185d4b77d5
Aleks-Frank <SACHAFRANK2004@yandex.ru>
2025-09-14 19:20:31 +0500

    Kotlin-Frank

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,3 +1,59 @@
 package model
 
-class Player
+class Player {
+    var namePlayer: String
+    var team: Team
+    var position: String
+    var nationality: String
+    var agency: String
+    var transferCost: Int
+    var countMatches: Int
+    var countGoals: Int
+    var countAssists: Int
+    var countYellowCards: Int
+    var countRedCards: Int
+
+    constructor(){
+        this.namePlayer = ""
+        this.team = Team("", "")
+        this.position = ""
+        this.nationality = ""
+        this.agency = ""
+        this.transferCost = 0
+        this.countMatches = 0
+        this.countGoals = 0
+        this.countAssists = 0
+        this.countYellowCards = 0
+        this.countRedCards = 0
+    }
+
+    constructor(namePlayer: String,
+                team: Team,
+                position: String,
+                nationality: String,
+                agency: String,
+                transferCost: Int,
+                countMatches: Int,
+                countGoals: Int,
+                countAssists: Int,
+                countYellowCards: Int,
+                countRedCards: Int){
+        this.namePlayer = namePlayer
+        this.team = team
+        this.position = position
+        this.nationality = nationality
+        this.agency = agency
+        this.transferCost = transferCost
+        this.countMatches = countMatches
+        this.countGoals = countGoals
+        this.countAssists = countAssists
+        this.countYellowCards = countYellowCards
+        this.countRedCards = countRedCards
+    }
+
+    override fun toString(): String {
+        return "Player(namePlayer='$namePlayer', team=$team, position='$position', nationality='$nationality', agency='$agency', transferCost=$transferCost, countMatches=$countMatches, countGoals=$countGoals, countAssists=$countAssists, countYellowCards=$countYellowCards, countRedCards=$countRedCards)"
+    }
+}
+
+
--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,42 @@
 package parser
 
-object CsvParser
+import java.io.File
+import model.Player
+import model.Team
+
+object CsvParser{
+    fun csvParser(filename: String): List<Player>{
+        val lines = File(filename).readLines()
+        if (lines.size < 2) return emptyList()
+
+        val result = mutableListOf<Player>()
+
+        for (i in 1 until lines.size){
+            val line = lines[i]
+            val columns = line.split(";").map { it.trim() }
+
+            if (columns.size >= 11){
+                try {
+                    val team = Team(columns[1], columns[2])
+                    val player = Player(
+                        namePlayer = columns[0],
+                        team = team,
+                        position = columns[3],
+                        nationality = columns[4],
+                        agency = columns[5],
+                        transferCost = columns[6].toInt(),
+                        countMatches = columns[7].toInt(),
+                        countGoals = columns[8].toInt(),
+                        countAssists = columns[9].toInt(),
+                        countYellowCards = columns[10].toInt(),
+                        countRedCards = columns[10].toInt()
+                    )
+                    result.add(player)
+                } catch (e : NumberFormatException) {
+                    println("Ошибка преобразования числа в строке ${i + 1}: $line")
+                }
+            }
+        }
+        return result
+    }
+}
