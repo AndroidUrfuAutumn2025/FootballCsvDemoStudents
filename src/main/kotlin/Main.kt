5c380615947f7acf1c5344e08fe2f1d95933399f
Gleb712 <glebmaskalev712@gmail.com>
2025-09-16 00:02:27 +0500

    Маскалев

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
-fun main(args: Array<String>) {
-    print("Yeah rock!")
+import parser.CsvParser
+import resolver.Resolver
+import visualization.ChartBuilder
+
+fun main() {
+    println("Загрузка данных...")
+
+    val players = CsvParser.parsePlayers("src/main/resources/fakePlayers.csv")
+    println("Загружено ${players.size} игроков")
+
+    val resolver = Resolver(players)
+
+    // Задания
+    println("\n1. Игроков без агентства: ${resolver.getCountWithoutAgency()}")
+
+    val bestDefender = resolver.getBestScorerDefender()
+    println("2. Лучший защитник: ${bestDefender.first} (голов: ${bestDefender.second})")
+
+    val germanPosition = resolver.getTheExpensiveGermanPlayerPosition()
+    println("3. Позиция самого дорогого немецкого игрока: $germanPosition")
+
+    val rudestTeam = resolver.getTheRudestTeam()
+    println("4. Самая грубая команда: ${rudestTeam.name}")
+
+    // Создаем диаграмму
+    println("\nСоздание диаграммы...")
+    ChartBuilder.createNationalityPieChart(players)
+
+    println("Готово!")
 }
