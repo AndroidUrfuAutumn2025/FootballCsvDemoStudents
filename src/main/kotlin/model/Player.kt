ddefba33cb1373c811aa9a5fda8f27b01e39013b
Irina <shurik_burn@mail.ru>
2025-09-15 18:35:56 +0500

    Задание 1

--- src/main/kotlin/model/Player.kt
+++ src/main/kotlin/model/Player.kt
@@ -1,3 +1,28 @@
 package model
+enum class Position(val russianName: String) {
+    MIDFIELD("Полузащитник"),
+    DEFENDER("Защитник"),
+    GOALKEEPER("Вратарь"),
+    FORWARD("Нападающий");
 
-class Player
+    companion object {
+        fun getByValue(s: String): Position {
+            return entries.find { it.name.equals(s, ignoreCase = true) }
+                ?: throw IllegalArgumentException("Unknown position: $s")
+        }
+    }
+}
+
+class Player(
+    val name: String,
+    val team: Team,
+    val position: Position,
+    val nationality: String,
+    val agency: String,
+    val transferCost: Int,
+    val participationCount: Int,
+    val goals: Int,
+    val assists: Int,
+    val yellowCards: Int,
+    val redCards: Int
+)
--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,42 @@
 package parser
 
-object CsvParser
+import model.Player
+import model.Position
+import model.Team
+import java.io.File
+
+object CsvParser {
+
+    private var players: MutableList<Player> = mutableListOf()
+
+    fun parse(path: String): List<Player> {
+        players.clear()
+        File(path).useLines { lines ->
+            lines.drop(1)
+                .forEach { parsePlayerFromString(it) }
+        }
+        return players
+    }
+
+    private fun parsePlayerFromString(playerString: String) {
+        val playerParts = playerString.split(";")
+
+        players.add(
+            Player(
+                name = playerParts[0],
+                team = Team(name = playerParts[1], city = playerParts[2]),
+                position = Position.getByValue(playerParts[3]),
+                nationality = playerParts[4],
+                agency = playerParts[5],
+                transferCost = playerParts[6].toInt(),
+                participationCount = playerParts[7].toInt(),
+                goals = playerParts[8].toInt(),
+                assists = playerParts[9].toIntOrNull().orZero(),
+                yellowCards = playerParts[10].toIntOrNull().orZero(),
+                redCards = playerParts[11].toIntOrNull().orZero()
+            )
+        )
+    }
+
+    private fun Int?.orZero() = this ?: 0
+}
