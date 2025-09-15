71f060721cd0cd8d24198999b3c7beabe1c26a15
Андрей Колчин <Andrei.Kolchin@urfu.me>
2025-09-15 09:58:49 +0500

    homework1 - done

--- src/main/kotlin/parser/CsvParser.kt
+++ src/main/kotlin/parser/CsvParser.kt
@@ -1,3 +1,32 @@
 package parser
+import java.io.InputStream
+import model.*
+import java.io.File
 
-object CsvParser
+
+object CsvParser{
+    const val PATH = "/home/user/study/Kotlin/KotlinHomework1/src/main/resources/fakePlayers.csv"
+
+    fun readCsv(): List<Player> {
+        val reader = File(PATH).inputStream().bufferedReader()
+        val header = reader.readLine()
+        return reader.lineSequence()
+            .map{
+                val data = it.split(";")
+                Player(data[0],
+                    Team(data[1],
+                        data[2]),
+                    enumValueOf<Position>(data[3]) ,
+                    data[4],
+                    data[5].ifBlank { null },
+                    data[6].toFloat(),
+                    data[7].toInt(),
+                    data[8].toInt(),
+                    data[9].toInt(),
+                    data[10].toInt(),
+                    data[11].toInt())
+            }.toList()
+
+    }
+    val myList = readCsv()
+}
