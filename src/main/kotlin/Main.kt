75a09a91773173f9b10a6b1c05a5fe72d905e7d9
GrishinaTatiana <nushagrishina@gmail.com>
2025-09-24 14:36:07 +0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,17 @@
 import java.io.File
 import parser.CsvParser
 import resolver.Resolver
-//import diagram.DiagramCreator
+
 
 fun main() {
     val players = CsvParser.parse(File("src/main/resources/fakePlayers.csv"))
     val resolver = Resolver(players)
-    //val diagramCreator = DiagramCreator(players)
 
-    println("Количество игроков без агентства: ${resolver.getCountWithoutAgency()}")
+
+    println("Игроки без агентства: ${resolver.getCountWithoutAgency()}")
 
     val (name, goals) = resolver.getBestScorerDefender()
-    println("Лучший защитник: $name с $goals голами")
+    println("Защитник: $name с $goals голами")
 
     val expensiveGermanPosition = resolver.getTheExpensiveGermanPlayerPosition()
     println("Позиция самого дорогого немецкого игрока: $expensiveGermanPosition")
@@ -19,5 +19,5 @@ fun main() {
     val rudestTeam = resolver.getTheRudestTeam()
     println("Команда с наибольшим средним числом красных карточек: ${rudestTeam.name}")
 
-    //diagramCreator.saveCountryDistributionChart()
+
 }
